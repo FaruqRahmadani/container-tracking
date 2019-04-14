daf573c05a20c0ac803bd739de485502821fd146
syauqiahmd <ahmadsyauqi@merahputih.id>
2019-04-15 02:01:17 +0800

    add date filter

--- resources/views/monitoring/index.blade.php
+++ resources/views/monitoring/index.blade.php
@@ -8,6 +8,22 @@
 		</div>
 	</div>
 	<div class="card-body">
+		<form class="mb-3" method="post" target="_blank" action="">
+			@csrf
+			<div class="form-row align-items-center">
+				<div class="col-auto">
+					<label class="sr-only">Tanggal Awal</label>
+					<input type="date" class="form-control mb-2" value="{{now()->toDateString()}}" name="tanggal_awal">
+				</div>
+				<div class="col-auto">
+					<label class="sr-only">Tanggal Akhir</label>
+					<input type="date" class="form-control mb-2" value="{{now()->toDateString()}}" name="tanggal_akhir">
+				</div>
+				<div class="col-auto">
+					<button type="submit" class="btn btn-primary mb-2"><i class="fa fa-print"></i> Cetak</button>
+				</div>
+			</div>
+		</form>
 		<div class="table-responsive">
 			<table class="table table-bordered dataTable" id="" width="100%" cellspacing="0">
 				<thead>

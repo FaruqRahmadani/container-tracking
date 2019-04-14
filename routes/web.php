33d1275612fe4eea3e375aca268ed30c4c932832
FaruqRahmadani <faruq.rahmadani@yahoo.com>
2019-04-15 00:28:57 +0800

    tampilan, controller, dan migration user

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,7 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
-    return view('template.dashboard');
+  return view('template.dashboard');
 });
 // ======== template pages =========
 // dashboard
@@ -38,4 +27,10 @@ Route::view('template/data', 'template.dataTable');
 // chart.js
 Route::view('template/chart', 'template.chart');
 
-Route::view('/login', 'depan.auth');
+Route::group(['namespace' => 'Auth'], function () {
+  Route::get('login', 'LoginController@showLoginForm');
+  Route::post('login', 'LoginController@login')->name('login');
+  Route::get('logout', 'LoginController@logout')->name('logout');
+});
+
+Route::get('/home', 'HomeController@index')->name('home');

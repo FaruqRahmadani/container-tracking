2547eef067823bab471540cd428977884e1ef433
FaruqRahmadani <faruq.rahmadani@yahoo.com>
2019-04-15 01:11:11 +0800

    faker rekanan

---
+++ database/factories/RekananFactory.php
@@ -0,0 +1,13 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Rekanan::class, function (Faker $faker) {
+  return [
+    'nama' => $faker->name(),
+    'alamat' => $faker->address,
+    'nomor_telepon' => $faker->phoneNumber,
+    'email' => $faker->safeEmail,
+    'penanggung_jawab' => $faker->name()
+  ];
+});

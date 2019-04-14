4a8821d62ad6b391de8107091cfb67cee3bde308
FaruqRahmadani <faruq.rahmadani@yahoo.com>
2019-04-15 00:37:28 +0800

    setup username menjadi username

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
   *
   * @var string
   */
-  protected $redirectTo = '/home';
+  protected $redirectTo = '/';
 
   /**
   * Create a new controller instance.
@@ -39,5 +39,9 @@ class LoginController extends Controller
 
   public function showLoginForm(){
     return view('auth.login');
+  }
+
+  public function username(){
+    return 'username';
   }
 }

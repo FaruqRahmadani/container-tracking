35b7489aed7f9e0c858a0b2b956a220600783ab3
FaruqRahmadani <faruq.rahmadani@yahoo.com>
2019-04-15 00:48:55 +0800

    fungsi login dan middlewarenya

--- routes/web.php
+++ routes/web.php
@@ -1,31 +1,33 @@
 <?php
 
-Route::get('/', function () {
-  return view('template.dashboard');
+Route::group(['middleware' => 'auth'], function(){
+  Route::get('/', function () {
+    return view('template.dashboard');
+  });
+  // ======== template pages =========
+  // dashboard
+  Route::view('template/dashboard', 'template.dashboard');
+  // auth pages
+  Route::view('template/login', 'template.authLogin');
+  Route::view('template/register', 'template.authRegister');
+  Route::view('template/forget', 'template.authForgetPassword');
+  // component
+  Route::view('template/button', 'template.button');
+  Route::view('template/card', 'template.card');
+  Route::view('template/form1', 'template.formTopLabel');
+  Route::view('template/form2', 'template.formHorizontal');
+  // utility
+  Route::view('template/color', 'template.utilityColor');
+  Route::view('template/border', 'template.utilityBorder');
+  Route::view('template/animation', 'template.utilityAnimation');
+  Route::view('template/other', 'template.utilityOther');
+  // pages
+  Route::view('template/blank', 'template.blank');
+  // dataTable
+  Route::view('template/data', 'template.dataTable');
+  // chart.js
+  Route::view('template/chart', 'template.chart');
 });
-// ======== template pages =========
-// dashboard
-Route::view('template/dashboard', 'template.dashboard');
-// auth pages
-Route::view('template/login', 'template.authLogin');
-Route::view('template/register', 'template.authRegister');
-Route::view('template/forget', 'template.authForgetPassword');
-// component
-Route::view('template/button', 'template.button');
-Route::view('template/card', 'template.card');
-Route::view('template/form1', 'template.formTopLabel');
-Route::view('template/form2', 'template.formHorizontal');
-// utility
-Route::view('template/color', 'template.utilityColor');
-Route::view('template/border', 'template.utilityBorder');
-Route::view('template/animation', 'template.utilityAnimation');
-Route::view('template/other', 'template.utilityOther');
-// pages
-Route::view('template/blank', 'template.blank');
-// dataTable
-Route::view('template/data', 'template.dataTable');
-// chart.js
-Route::view('template/chart', 'template.chart');
 
 Route::group(['namespace' => 'Auth'], function () {
   Route::get('login', 'LoginController@showLoginForm');
